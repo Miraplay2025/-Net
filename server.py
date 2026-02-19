10813fc1d5a7d1abee9573f702b8ab8041712a9e
Miraplay2025 <massingueantoniocarlos38@gmail.com>
2026-02-19 22:01:09 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,4 @@
-import http.server
-import socketserver
-import socket
-import select
+import http.server, socketserver, socket, select
 
 class Proxy(http.server.SimpleHTTPRequestHandler):
     def do_CONNECT(self):
@@ -9,24 +6,20 @@ class Proxy(http.server.SimpleHTTPRequestHandler):
         address[1] = int(address[1])
         try:
             s = socket.create_connection(address, timeout=10)
-        except Exception:
-            self.send_error(502)
-            return
+        except:
+            self.send_error(502); return
         self.send_response(200, 'Connection Established')
         self.end_headers()
         conns = [self.connection, s]
         while True:
-            read_ready, _, _ = select.select(conns, [], [], 10)
-            if not read_ready:
-                break
-            for r in read_ready:
+            r_ready, _, _ = select.select(conns, [], [], 10)
+            if not r_ready: break
+            for r in r_ready:
                 other = conns[1] if r is conns[0] else conns[0]
                 data = r.recv(8192)
-                if not data:
-                    return
+                if not data: return
                 other.sendall(data)
 
 PORT = 8888
-print(f"Servidor iniciado na porta {PORT}")
-with socketserver.ThreadingTCPServer(("", PORT), Proxy) as httpd:
-    httpd.serve_forever()
+print(f"Proxy ON na porta {PORT}")
+socketserver.ThreadingTCPServer(("", PORT), Proxy).serve_forever()
